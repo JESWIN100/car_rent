7844d8f3bb0806157f27cd22309b467c88e1e95e
JESWIN100 <josepjjeswin20@gmail.com>
2024-09-04 12:13:33 +0530

    deploy2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: "https://morent-jeswins-projects-f40474fd.vercel.app",
     credentials:true,
 })); // Enable CORS
 app.use(bodyParser.json()); // Parse JSON bodies
