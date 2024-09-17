13f80e0df0faf50f621b1c9fc0a8533ddea972fa
JESWIN100 <josepjjeswin20@gmail.com>
2024-09-17 23:57:08 +0530

    originalsow

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ connectDB();
 app.use(cors({
     origin: "https://car-rental-frontent-ten.vercel.app", 
     credentials: true, 
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: [
       "Origin",
       "Content-Type",
